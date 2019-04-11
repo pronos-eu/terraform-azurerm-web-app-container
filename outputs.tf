7e33b43e55b65857ab1bb811c37d599fec43b7ac
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2019-04-11 11:18:23 +0200

    feat: add system-assigned identity
    Use a managed identity for the App Service

--- outputs.tf
+++ outputs.tf
@@ -21,3 +21,8 @@ output "possible_outbound_ips" {
 
   description = "A list of possible outbound IP addresses for the App Service. Superset of outbound_ips."
 }
+
+output "principal_id" {
+  value       = "${azurerm_app_service.main.identity.0.principal_id}"
+  description = "The principal ID for the system-assigned identity associated with the App Service."
+}
